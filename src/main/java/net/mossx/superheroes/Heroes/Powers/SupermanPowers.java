f688b74547dc31ed68db6c6fbf35f73848990e00
Moss <63621656+mossx420@users.noreply.github.com>
2021-03-15 16:39:30 -0400

    Added FrostBreath Power

--- src/main/java/net/mossx/superheroes/Heroes/Powers/SupermanPowers.java
+++ src/main/java/net/mossx/superheroes/Heroes/Powers/SupermanPowers.java
@@ -8,14 +8,21 @@ import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.entity.Damageable;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.RayTraceResult;
 
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class SupermanPowers extends HeroPowers {
     public boolean heatVision = false;
@@ -34,6 +41,27 @@ public class SupermanPowers extends HeroPowers {
             }
         }).runTaskLater(Superheroes.plugin, 5L);
     }
+    public static void FrostBreath(Player p) {
+        for (double j = 0; j < 6; j+=.5) {
+            Location particleLocation = p.getEyeLocation().add(p.getEyeLocation().getDirection().multiply(j + 1));
+            particleLocation.getWorld().spawnParticle(Particle.SNOWBALL, particleLocation, 1, new Particle.DustOptions(Color.RED, 0.4F));
+        }
+        RayTraceResult ray = p.getWorld().rayTraceEntities(p.getEyeLocation(), p.getEyeLocation().getDirection(), 100, 4, new Predicate<Entity>() {
+            @Override
+            public boolean test(Entity entity) {
+                if (entity instanceof Player) {
+                    return !(entity.getUniqueId().equals(p.getUniqueId()));
+                }
+                return true;
+            }
+        });
+        if (ray != null) {
+            Entity e = ray.getHitEntity();
+            if (e instanceof LivingEntity) {
+                ((LivingEntity) e).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 2, 2, false, false));
+            }
+        }
+    }
 
 
 
@@ -42,7 +70,9 @@ public class SupermanPowers extends HeroPowers {
         Leggings(new ItemStack(Material.LEATHER_LEGGINGS), HeroPowers.inv.setColor((LeatherArmorMeta)new ItemStack(Material.LEATHER_LEGGINGS).getItemMeta(), Color.fromRGB(44, 180, 2)), 1),
         Boots(new ItemStack(Material.LEATHER_BOOTS), HeroPowers.inv.setColor((LeatherArmorMeta)new ItemStack(Material.LEATHER_BOOTS).getItemMeta(), Color.YELLOW), 2),
 
-        HeatVision(Hero.createPower(Material.RED_DYE, "Heat Vision"), 8, SupermanPowers::HeatVision)
+        HeatVision(Hero.createPower(Material.RED_DYE, "Heat Vision"), 8, SupermanPowers::HeatVision),
+        FrostBreath(Hero.createPower(Material.SNOW, "Frost Breath"), 8, SupermanPowers::FrostBreath)
+
         ;
 
         ItemStack stack; int position; Consumer<Player> run;
